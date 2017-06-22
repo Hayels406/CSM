2672217b2e444bb23cf54f77f57dcd3cdad0ae0b
Hayley Moore <hayley@orange>
2017-06-22 11:31:14 +0100

    scripts to run diagnosics straight after CSM.py

---
+++ doCSM.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+python CSM.py
+mkdir -p frames
+rm frames/*
+python ~/Documents/CSM/plotFrames.py
+p=`pwd`
+vid=`echo ${p##*/}`
+ffmpeg -framerate 15 -pattern_type glob -i 'frames/*.png' -vcodec mpeg4 -qscale:v 2 frames/${vid}.mp4
+mkdir -p plots
+python ~/Documents/CSM/plotSheepAcceleration.py
+python ~/Documents/CSM/plotSheepVelocity.py
+python ~/Documents/CSM/plotDogAcceleration.py
+python ~/Documents/CSM/plotDogVelocity.py
+python ~/Documents/CSM/plotTimeStep.py
---
+++ plots.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+mkdir -p plots
+python ~/Documents/CSM/plotSheepAcceleration.py
+python ~/Documents/CSM/plotSheepVelocity.py
+python ~/Documents/CSM/plotDogAcceleration.py
+python ~/Documents/CSM/plotDogVelocity.py
+python ~/Documents/CSM/plotTimeStep.py
