fcdf70d8e52598e9a6ba84763d0ab3def911da37
Hayley Moore <hayley@orange>
2017-09-06 19:57:24 +0100

    updated

--- doCSM.sh
+++ doCSM.sh
@@ -11,6 +11,11 @@ p=`pwd`
 vid=`echo ${p##*/}`
 ffmpeg -framerate 10 -pattern_type glob -i 'frames/*.png' -vcodec mpeg4 -qscale:v 2 frames/${vid}.mp4
 
+MAC=/Users/hayleymoore/Documents/Uni/Documents/CSM
+if [ -f $MAC/params.py.dist ]; then
+  open frames/${vid}.mp4
+fi
+
 mkdir -p plots
 mkdir -p output
 python ~/Documents/Uni/Documents/CSM/plotSheepAcceleration.py
@@ -20,9 +25,9 @@ python ~/Documents/Uni/Documents/CSM/plotDogVelocity.py
 python ~/Documents/Uni/Documents/CSM/plotTimeStep.py
 python ~/Documents/Uni/Documents/CSM/plotConvexHull.py
 python ~/Documents/Uni/Documents/CSM/plotVoronoiDistances.py
+python ~/Documents/Uni/Documents/CSM/findClusters.py
 
-
-MAC=/Users/hayleymoore/Documents/Uni/Documents/CSM
 if [ -f $MAC/params.py.dist ]; then
   terminal-notifier -title "CSM" -message "Done with simulation" -subtitle ${vid} -sound 'glass'
+  open frames/${vid}.mp4
 fi
