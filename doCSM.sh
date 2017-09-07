d71818c69cfd5a379493338697202a159f6b8098
Hayley Moore <hayley@orange>
2017-09-07 16:13:35 +0100

    modified do

--- doCSM.sh
+++ doCSM.sh
@@ -3,26 +3,6 @@ INSTALLDIR=/home/b1033128/Documents/CSM
 INSTALLDIR_TOPSY=/home/b1033128/CSM
 INSTALLDIR_HOME=/Users/hayleymoore/Documents/Uni/Documents/CSM
 
-rm -f *.h5
-rm -f *.pyc
-
-python -b CSM.py
-
-mkdir -p frames
-rm -f frames/*
-python ~/Documents/Uni/Documents/CSM/plotFrames.py
-p=`pwd`
-vid=`echo ${p##*/}`
-ffmpeg -framerate 10 -pattern_type glob -i 'frames/*.png' -vcodec mpeg4 -qscale:v 2 frames/${vid}.mp4
-
-MAC=/Users/hayleymoore/Documents/Uni/Documents/CSM
-if [ -f $MAC/params.py.dist ]; then
-  open frames/${vid}.mp4
-fi
-
-mkdir -p plots
-mkdir -p output
-
 if [ -f $INSTALLDIR/params.py.dist ]; then
 	DIR=$INSTALLDIR
 fi
@@ -33,6 +13,25 @@ if [ -f $INSTALLDIR_HOME/params.py.dist ]; then
 	DIR=$INSTALLDIR_HOME
 fi
 
+rm -f *.h5
+rm -f *.pyc
+
+python -b ~/CSM/CSM.py
+
+mkdir -p frames
+rm -f frames/*
+python $DIR/plotFrames.py
+p=`pwd`
+vid=`echo ${p##*/}`
+ffmpeg -framerate 10 -pattern_type glob -i 'frames/*.png' -vcodec mpeg4 -qscale:v 2 frames/${vid}.mp4
+
+MAC=$INSTALLDIR_HOME
+if [ -f $MAC/params.py.dist ]; then
+  open frames/${vid}.mp4
+fi
+
+mkdir -p plots
+mkdir -p output
 
 python $DIR/plotSheepAcceleration.py
 python $DIR/plotSheepVelocity.py
