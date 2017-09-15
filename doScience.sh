b96b947fc0fd0d1091718d65fe5de51d9f20f60a
Hayley Moore <hayley@orange>
2017-09-15 09:42:05 +0100

    extract info from data

---
+++ doScience.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+INSTALLDIR=/home/b1033128/Documents/CSM
+INSTALLDIR_TOPSY=/home/b1033128/CSM
+INSTALLDIR_HOME=/Users/hayleymoore/Documents/Uni/Documents/CSM
+
+if [ -f $INSTALLDIR/params.py.dist ]; then
+	DIR=$INSTALLDIR
+fi
+if [ -f $INSTALLDIR_TOPSY/params.py.dist ]; then
+	DIR=$INSTALLDIR_TOPSY
+fi
+if [ -f $INSTALLDIR_HOME/params.py.dist ]; then
+	DIR=$INSTALLDIR_HOME
+fi
+
+HERE=pwd
+for wall in Sqaure Circular; do
+	echo ls
