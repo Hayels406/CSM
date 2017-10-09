2f0cfce098ba5fface24624982548d653faeb9ca
Hayley Moore <hayley@orange>
2017-10-09 09:36:35 +0100

    removed script

--- plots.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-mkdir -p plots
-python ~/Documents/CSM/plotSheepAcceleration.py
-python ~/Documents/CSM/plotSheepVelocity.py
-python ~/Documents/CSM/plotDogAcceleration.py
-python ~/Documents/CSM/plotDogVelocity.py
-python ~/Documents/CSM/plotTimeStep.py
