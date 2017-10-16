f6f7299232e6b0fd818a9041f0d8dca5ff08b224
Hayley Moore <hayley@orange>
2017-10-16 10:09:48 +0100

    plots simplified

--- plotAlpha.py
+++ plotAlpha.py
@@ -18,11 +18,8 @@ if topsy == False:
     rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
     rc('text', usetex=True)
 
-filesSquare = glob('Square/pred*/group*/output/alpha')
+filesSquare = glob('./pred*/group*/output/alpha')
 filesSquare.sort()
-
-filesCircular = glob('Circular/pred*/group*/output/alpha')
-filesCircular.sort()
 
 k = np.array([])
 Ni = np.array([])
@@ -59,43 +56,3 @@ for i in range(len(set(k))):
 	plt.title(r'$k = '+str(kValue)+'$', fontsize = 20)
 	plt.savefig('./Square/pred'+str(kValue).zfill(3)+'/alpha.png')
 	plt.close()
-
-
-print 'Circular'
-
-k = np.array([])
-Ni = np.array([])
-alpha = np.array([])
-for fileLoc in filesCircular:
-	fileLoc[fileLoc.rfind('/group')]
-	k = np.append(k, int(fileLoc[fileLoc.rfind('pred')+4:fileLoc.rfind('/group')]))
-	Ni = np.append(Ni, int(fileLoc[fileLoc.rfind('group')+5:fileLoc.rfind('output')-1]))
-	file = open(fileLoc,'r')
-	betaValue = float(file.read())
-	file.close()
-	alpha = np.append(alpha, betaValue)
-
-k = np.array(map(int, k))
-Ni = np.array(map(int, Ni))
-alpha = alpha.reshape(len(Ni)/((Ni == 500.).sum()), (Ni == 500.).sum())
-
-
-plt.pcolor(alpha)
-plt.colorbar()
-plt.xlabel('Pred Neighbours')
-plt.ylabel('Prey Neighbours')
-ax = plt.gca()
-ax.set_xticks(np.array(range(len(list(set(k))))) + 0.5)
-ax.set_xticklabels(sorted(list(set(k))))
-ax.set_yticks(np.array(range(len(list(set(Ni))))) + 0.5)
-ax.set_yticklabels(sorted(list(set(Ni))))
-plt.savefig('./Circular/pcolorAlpha.png')
-plt.close()
-
-for i in range(len(set(k))):
-	kValue = np.array(sorted(list(set(k))))[i]
-	plt.plot(Ni[k == kValue], alpha[:, i])
-	plt.xlabel('Prey Neighbours', fontsize = 18)
-	plt.ylabel(r'$\alpha$', fontsize = 18)
-	plt.title(r'$k = '+str(kValue)+'$', fontsize = 20)
-	plt.savefig('./Circular/pred'+str(kValue).zfill(3)+'/alpha.png')
--- plotBeta.py
+++ plotBeta.py
@@ -18,11 +18,8 @@ if topsy == False:
     rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
     rc('text', usetex=True)
 
-filesSquare = glob('Square/pred*/group*/output/beta')
+filesSquare = glob('./pred*/group*/output/beta')
 filesSquare.sort()
-
-filesCircular = glob('Circular/pred*/group*/output/beta')
-filesCircular.sort()
 
 k = np.array([])
 Ni = np.array([])
@@ -59,41 +56,3 @@ for i in range(len(set(k))):
 	plt.title(r'$k = '+str(kValue)+'$', fontsize = 20)
 	plt.savefig('./Square/pred'+str(kValue).zfill(3)+'/beta.png')
 	plt.close()
-
-
-k = np.array([])
-Ni = np.array([])
-beta = np.array([])
-for fileLoc in filesCircular:
-	fileLoc[fileLoc.rfind('/group')]
-	k = np.append(k, int(fileLoc[fileLoc.rfind('pred')+4:fileLoc.rfind('/group')]))
-	Ni = np.append(Ni, int(fileLoc[fileLoc.rfind('group')+5:fileLoc.rfind('output')-1]))
-	file = open(fileLoc,'r')
-	betaValue = float(file.read())
-	file.close()
-	beta = np.append(beta, betaValue)
-
-k = np.array(map(int, k))
-Ni = np.array(map(int, Ni))
-beta = beta.reshape(len(Ni)/((Ni == 500.).sum()), (Ni == 500.).sum())
-
-
-plt.pcolor(beta)
-plt.colorbar()
-plt.xlabel('Pred Neighbours')
-plt.ylabel('Prey Neighbours')
-ax = plt.gca()
-ax.set_xticks(np.array(range(len(list(set(k))))) + 0.5)
-ax.set_xticklabels(sorted(list(set(k))))
-ax.set_yticks(np.array(range(len(list(set(Ni))))) + 0.5)
-ax.set_yticklabels(sorted(list(set(Ni))))
-plt.savefig('./Circular/pcolorBeta.png')
-plt.close()
-
-for i in range(len(set(k))):
-	kValue = np.array(sorted(list(set(k))))[i]
-	plt.plot(Ni[k == kValue], beta[:, i])
-	plt.xlabel('Prey Neighbours', fontsize = 18)
-	plt.ylabel(r'$\beta$', fontsize = 18)
-	plt.title(r'$k = '+str(kValue)+'$', fontsize = 20)
-	plt.savefig('./Circular/pred'+str(kValue).zfill(3)+'/beta.png')
