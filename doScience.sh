5db30696b122a06ce442e41fa977c2a62f15a9e1
Hayley Moore <hayley@orange>
2017-10-16 09:45:39 +0100

    noise if statement

--- doScience.sh
+++ doScience.sh
@@ -16,25 +16,27 @@ fi
 HERE=$(pwd)
 for wall in Square* Circular*; do
 	cd $wall
-	for noise in noise*; do
-		cd $noise
-		for pred in pred*; do
-			cd $pred
-			for ni in group[0-9]*; do
-				cd $ni
-				echo $(pwd)
-				#python -b $DIR/getStatistics.py
-				#python -b $DIR/getAlphaBeta.py
+	if [ -f $wall/noise1.0 ]; then
+		for noise in noise*; do
+			cd $noise
+			for pred in pred*; do
+				cd $pred
+				for ni in group[0-9]*; do
+					cd $ni
+					echo $(pwd)
+					#python -b $DIR/getStatistics.py
+					#python -b $DIR/getAlphaBeta.py
+					cd ..
+				done
+				#python -b $DIR/plotStats.py
+				#python -b $DIR/plotAlpha.py
+				#python -b $DIR/plotBeta.py
+				#python -b $DIR/plotGroupPredation.py
 				cd ..
 			done
-			#python -b $DIR/plotStats.py
-			#python -b $DIR/plotAlpha.py
-			#python -b $DIR/plotBeta.py
-			#python -b $DIR/plotGroupPredation.py
 			cd ..
 		done
-		cd ..
-	done
+	fi
 	for pred in pred*; do
 		cd $pred
 		for ni in group[0-9]*; do
